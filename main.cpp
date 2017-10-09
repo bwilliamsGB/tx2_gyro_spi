c92e00183ef7b2881620015b5ae3a407ea04ab4c
Brian Williams <brian.fitch.williams@gmail.com>
2017-10-09 14:34:53 +0000

    Read mag working with noisy data, gyro fixed value

--- main.cpp
+++ main.cpp
@@ -15,15 +15,25 @@ int main(int argc, char** argv)
     uint16_t retval = gyro.begin();
     cout << "Gyro begin return value: " << retval << endl;
 
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < 100; i++) {
         // To read from the gyroscope, you must first call the
         // readGyro() function. When this exits, it'll update the
         // gx, gy, and gz variables with the most current data.
         gyro.readGyro();
-        cout << "Read X: " << hex << gyro.gx <<
-                ", Y: " << hex << gyro.gy <<
-                ", Z: " << hex << gyro.gz << endl;
+        cout << "Calculated Gyro xyz: " << hex << gyro.calcMag(gyro.gx) <<
+                " " << hex << gyro.calcMag(gyro.gy) <<
+                " " << hex << gyro.calcMag(gyro.gz) << endl;
+        cout << "Raw Gyro xyz: " << hex << gyro.gx <<
+                " " << hex << gyro.gy <<
+                " " << hex << gyro.gz << endl;
 
+        gyro.readMag();
+        cout << "Calculated Mag x: " << hex << gyro.calcMag(gyro.mx) <<
+                " " << hex << gyro.calcMag(gyro.my) <<
+                " " << hex << gyro.calcMag(gyro.mz) << endl;
+        cout << "Raw Mag x: " << hex << gyro.mx <<
+                " " << hex << gyro.my <<
+                " " << hex << gyro.mz << endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
